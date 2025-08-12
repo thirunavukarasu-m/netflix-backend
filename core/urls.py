6c117176ace0d59125a95d44945751bb56a73268
me-thirunavukarasu <68892257+thirunavukarasu-m@users.noreply.github.com>
2025-08-12 23:10:17 +0530

    Added a landing page.

--- core/urls.py
+++ core/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import home
 
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('ping/', include('ping.urls')),
     path('auth/', include('users.urls')),
